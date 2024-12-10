83927c272f336ccdd0fa93c5b907d2baa4ed6202
Natalie <natalieannam@gmail.com>
2024-12-11 08:42:14 +1100

    Update Glycemia.java

--- Java/AccessControlModifiers/Glycemia.java
+++ Java/AccessControlModifiers/Glycemia.java
@@ -1,1 +1,11 @@
+/* This program demonstrates class declaration and access control principles,
+* and includes instances of Cell, Glucose, and Insulin classes. Glycemia is the superclass. */
 
+class Glycemia {
+	public static void main(String[] args) {
+		// create instance of Cell
+		// create instance of Glucose
+		// create instance of Insulin
+		
+	}
+}
