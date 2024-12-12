e6d0188ad061d37bfa0e951cc4565d49ce409a05
Natalie <natalieannam@gmail.com>
2024-12-13 04:13:22 +1100

    Update Cell.java

--- Java/AccessControlModifiers/Cell.java
+++ Java/AccessControlModifiers/Cell.java
@@ -1,1 +1,12 @@
 // Has insulin receptor, glucose channel.
+
+class Cell {
+
+	int glucose;
+	boolean insulinReceptor;
+
+	public Cell(int glu, boolean insulinRec) {
+		glucose = glu;
+		insulinReceptor = insulinRec;
+	}
+}
