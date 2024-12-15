1692f10aa7f198883a372390daa94b10197f891b
Natalie <natalieannam@gmail.com>
2024-12-15 21:10:11 +1100

    Update Glycemia.java
    Creating packages.

--- Java/AccessControlModifiers/Glycemia.java
+++ Java/AccessControlModifiers/Glycemia.java
@@ -1,12 +1,20 @@
 /* This program demonstrates class declaration and access control principles,
-* and includes instances of Cell, Glucose, and Insulin classes. Glycemia is the superclass. */
+* and includes instances of Cell, Glucose, and Insulin classes. Glycemia is the superclass. 
+* To create package:
+*	javac Glycemia.java
+*	javac -d . Glycemia.java
+*	Java GlycemiaSuperclass.Glycemia
+*/
+package GlycemiaSuperclass;
+
+//import GlycemiaSubclasses.Cell;
 
 class Glycemia {
 	public static void main(String[] args) {
 		// create instance of Cell
-		Cell cell1 = new Cell(5,false);
-		System.out.println("Glucose: " + cell1.glucose);
-		System.out.println("Insulin Receptor (open?): " + cell1.insulinReceptor);
+		//Cell cell1 = new Cell(5,false);
+		//System.out.println("Glucose: " + cell1.glucose);
+		//System.out.println("Insulin Receptor (open?): " + cell1.insulinReceptor);
 		// create instance of Glucose
 		// create instance of Insulin
 	}
